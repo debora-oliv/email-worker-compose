fceda15629ed6fcd63d75ca666d18f7fe018bd05
Débora Alves <deboraalves007@hotmail.com>
2025-08-04 22:04:55 -0300

    add function to register emails & updated send function

--- app/sender.py
+++ app/sender.py
@@ -1,7 +1,28 @@
+import pyscopg2
 from bottle import route, run, request
 
+DSN = 'dbname=email_sender user=postgres host=db'
+SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
+
+def register_email(assunto, mensagem):
+    try:
+        conn = pyscopg2.connect(DSN)
+        cursor = conn.cursor()
+        cursor.execute(SQL, (assunto, mensagem))
+        conn.commit()
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        cursor.close()
+        conn.close()
+        print("Email registrado!")
+
 @route('/api', method='POST')
-def send():    
+def send():   
+    assunto = request.forms.get('assunto')
+    mensagem = request.forms.get('mensagem')
+
+    register_email(assunto, mensagem)
     return "Email com assunto enviado com sucesso!"
 
 if __name__ == '__main__':
